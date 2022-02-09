260e939b4e407b3047ef822048883aaf8292c5ba
ericckendrick <eric.c.kendrick@gmail.com>
2022-02-09 15:02:43 -0600

    Changed 99 lines Pt 1 to function from DOM manipulation, removed previous html, updated readme file

--- app.js
+++ app.js
@@ -1,53 +1,77 @@
-// Create an array of five friends
-let friends = ["Brent", "Haley", "Ben", "Becca", "Eric"];
 
-// Assign variable with each name and loop through song. 
+document.addEventListener("DOMContentLoaded", singEvent);
 
-for (let i = 0; i < friends.length; i++) {
-    // Create var for each individual friend
-    let friend = friends[i];
-    console.log(friend +": ")
-    
+function singEvent() {
 
-        for (let j = 99; j > 0; j--) {
-            if (j > 2) {
-                console.log(
-                j +
-                    " " +
-                    "lines of code in the file," +
-                    " " +
-                    j +
-                    " " +
-                    "lines of code; " + friend + " strikes one out, clears it all out," +
-                    " " +
-                    (j - 1) +
-                    " " +
-                    "lines of code in the file."
-                );
-            } else if (j === 2) {
-                console.log(
-                j +
-                    " " +
-                    "lines of code in the file," +
-                    " " +
-                    j +
-                    " " +
-                    "lines of code; " + friend + " strikes one out, clears it all out," +
-                    " " +
-                    (j - 1) +
-                    " " +
-                    "line of code in the file."
-                );
-            } else {
-                console.log(
-                j +
-                    " " +
-                    "line of code in the file," +
-                    " " +
-                    j +
-                    " " +
-                    "line of code; " + friend + " strikes it out, clears it all out, and no more lines of code in the file!"
-                );
-            };
-        };
-};
+        let singBtn = document.createElement('button');
+        let singBtnText = document.createTextNode('Sing!');
+        singBtn.append(singBtnText);
+        document.body.appendChild(singBtn);
+        
+        function onClickSing() {
+        // Create an array of five friends
+        let friends = ["Brent", "Haley", "Ben", "Becca", "Eric"];
+
+        // Assign variable with each name and loop through song. 
+        for (let i = 0; i < friends.length; i++) {
+            //---Create div with class 'friend' for each friend----//
+            let friendDiv = document.createElement('div');
+            friendDiv.className = 'friend';
+            document.body.appendChild(friendDiv);
+            //------h3 tag with friend's name-------//
+            let friendHead = document.createElement('h3');
+            friendHead.innerText = friends[i] + ": ";
+            //---Append h tag to friend div ------//
+            let friendPara = document.getElementsByClassName('friend');
+            friendPara[i].appendChild(friendHead);
+
+            //----Para element to contain each lyric line----//
+            
+                for (let j = 99; j > 0; j--) {
+                    var lyric = document.createElement('p');
+            
+                    if (j > 2) {
+                        lyric.innerText = (
+                        j +
+                            " " +
+                            "lines of code in the file," +
+                            " " +
+                            j +
+                            " " +
+                            "lines of code; " + friends[i] + " strikes one out, clears it all out," +
+                            " " +
+                            (j - 1) +
+                            " " +
+                            "lines of code in the file."
+                        );
+                    } else if (j === 2) {
+                        lyric.innerText = (
+                        j +
+                            " " +
+                            "lines of code in the file," +
+                            " " +
+                            j +
+                            " " +
+                            "lines of code; " + friends[i] + " strikes one out, clears it all out," +
+                            " " +
+                            (j - 1) +
+                            " " +
+                            "line of code in the file."
+                        );
+                    } else {
+                        lyric.innerText = (
+                        j +
+                            " " +
+                            "line of code in the file," +
+                            " " +
+                            j +
+                            " " +
+                            "line of code; " + friends[i] + " strikes it out, clears it all out, and no more lines of code in the file!"
+                        );
+                    };
+                    friendPara[i].appendChild(lyric);
+                };
+            }
+        }
+        singBtn.addEventListener('click', onClickSing);
+}
